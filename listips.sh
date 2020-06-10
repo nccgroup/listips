79a35503827266e34d1caf72c8c3e3c2ea98d2af
James conlan <james.conlan@nccgroup.com>
2020-06-10 10:24:43 +0100

    Added exclusion functionality

--- listips.sh
+++ listips.sh
@@ -3,25 +3,50 @@
 function help {
 	echo "Convert IP ranges to a list of IPs"
 	echo ""
-	echo "Usage: listips [-h] [-f filename] [IP range]"
+	echo "Usage: listips [-h] [-e IP range] [-ef filename] [IP range | -f filename]"
 	echo ""
-	echo "-f/--file: Specify a file containing IP ranges to convert"
-	echo "-h/--help: Show this help info"
+	echo "-e/--excl:  Specify an IP range to exclude"
+	echo "-ef/--exfl: Specify a file containing IP ranges to exclude"
+	echo "-f/--file:  Specify a file containing IP ranges to convert"
+	echo "-h/--help:  Show this help info"
 	exit
 }
 
+cmd='nmap -n -sL'
+nextarg=1
+skip=false
+
 if [ $# -gt 0 ]; then
-	case "$1" in
-		--file|-f)
-			nmap -n -sL -iL $2 | awk '/Nmap scan report/{print $NF}'
-			;;
-		--help|-h)
-			help	
-			;;
-		*)
-			nmap -n -sL $1 | awk '/Nmap scan report/{print $NF}'
-			;;
-	esac
+	for arg in $@; do
+		nextarg=$((nextarg + 1))
+		if [ $skip = false ]; then
+			case $arg in
+				--excl|-e)
+					cmd="$cmd --exclude \$$nextarg"
+					skip=true
+					;;
+				--exfl|-ef)
+					cmd="$cmd --excludefile \$$nextarg"
+					skip=true
+					;;
+				--file|-f)
+					cmd="$cmd -iL \$$nextarg"
+					break
+					;;
+				--help|-h)
+					help	
+					;;
+				*)
+					cmd="$cmd $arg"
+					break
+					;;
+			esac
+		else
+			skip=false
+		fi
+	done
+	cmd="$cmd | awk '/Nmap scan report/{print \$NF}'"
+	eval $cmd
 else 
 	help
 fi
